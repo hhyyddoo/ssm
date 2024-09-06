3ae2c92de0940c34b3a6e375e8c91dd3acc3f17b
hhh <123@qq.com>
2024-09-06 10:00:49 +0800

    spring入门

---
+++ spring01-quickstart/src/main/java/com/hyd/tool/Tool.java
@@ -0,0 +1,10 @@
+package com.hyd.tool;
+
+import java.time.LocalDateTime;
+
+public class Tool {
+    public void getDate() {
+        LocalDateTime now = LocalDateTime.now();
+        System.out.println(now);
+    }
+}
